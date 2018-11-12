2c3cf689a67d31525d82e85964a8d9f371eba530
mticdud <39734673+mticdud@users.noreply.github.com>
2018-11-12 11:31:53 -0500

    Update multi_cell_curve.m

--- multi_cell_curve.m
+++ multi_cell_curve.m
@@ -29,8 +29,8 @@ Options.GIterations=0; %Number of GVF iterations, default 0 %400 before
 
 %{
 %Make Curvature Video with Masks:
-Curve_Video(5) = struct('cdata',[],'colormap',[]);
-for i=1:5
+Curve_Video(out) = struct('cdata',[],'colormap',[]);
+for i=1:out
     close all
     disp('Executing Snake Method Frame:')
     disp(i)
@@ -58,8 +58,8 @@ close(y)
 %}
 
 %Make Curvature Video with Raw Cell Images:
-Curve_Video(10) = struct('cdata',[],'colormap',[]);
-for i=1:10
+Curve_Video(out2) = struct('cdata',[],'colormap',[]);
+for i=1:out2
     close all
     disp('Executing Snake Method Frame:')
     disp(i)
